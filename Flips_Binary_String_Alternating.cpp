06ad3f93a43b7ace0c5546cb54250acf0fd8a5df
Adiba829 <144104759+Adiba829@users.noreply.github.com>
2026-03-07 14:56:39 +0530

    Minimum Number of Flips to Make the Binary String Alternating

---
+++ Flips_Binary_String_Alternating.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <climits>
+using namespace std;
+
+int minFlips(string s) {
+    int n = s.size();
+    string s2 = s + s;
+
+    string alt1 = "", alt2 = "";
+
+    for (int i = 0; i < 2*n; i++) {
+        alt1 += (i%2 ? '1':'0');
+        alt2 += (i%2 ? '0':'1');
+    }
+
+    int diff1 = 0, diff2 = 0;
+    int ans = INT_MAX;
+
+    for (int i = 0; i < 2*n; i++) {
+
+        if (s2[i] != alt1[i]) diff1++;
+        if (s2[i] != alt2[i]) diff2++;
+
+        if (i >= n) {
+            if (s2[i-n] != alt1[i-n]) diff1--;
+            if (s2[i-n] != alt2[i-n]) diff2--;
+        }
+
+        if (i >= n-1)
+            ans = min(ans, min(diff1, diff2));
+    }
+
+    return ans;
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << minFlips(s);
+
+    return 0;
+}
