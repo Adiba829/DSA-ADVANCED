c72f5457d93ed91f7f41628c0cebda3500f3cbb7
ADIBA <144104759+Adiba829@users.noreply.github.com>
2026-03-11 21:31:08 +0530

    Complement of Base 10 Integer

---
+++ Complement_of_Base_10_Integer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int bitwiseComplement(int n) {
+
+    if (n == 0) return 1;
+
+    int mask = 0;
+    int temp = n;
+
+    while (temp) {
+        mask = (mask << 1) | 1;
+        temp >>= 1;
+    }
+
+    return mask ^ n;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    cout << bitwiseComplement(n);
+
+    return 0;
+}
